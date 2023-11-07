0abb33f42bdb9f443686e001bfc6a9635e68a25a
fouzan0405 <fouzan0405gmail.com>
2023-11-07 12:26:45 +0530

    ver2

---
+++ lint.py
@@ -0,0 +1,16 @@
+"""
+This script adds two numbers, A and B, and prints the result.
+"""
+
+# Define the first number
+A = 1
+
+# Define the second number
+B = 2
+
+# Calculate the sum of A and B
+RESULT = A - B
+
+# Print the result
+print(RESULT)  # Print the sum of A and B
+print("hi")
